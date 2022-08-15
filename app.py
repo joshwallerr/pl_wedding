bef2b29901a70fdc687b4128d3a284cd6979440b
joshwallerr <joshwaller101@hotmail.com>
2022-08-16 00:02:19 +0100

    attempted a few things, nothing finished, just copied and pasted some previous code and will continue for next commit

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, make_response
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    render_template("index.html")
+    message = {'message': "Have a great day!", 'from': "Josh"}
+    return render_template("index.html", message=message)
+
+
+@app.route("/submit")
+def submit():
+    return render_template("submit.html")
+
+
+@app.route("/submit/process", methods=["POST", "GET"])
+def submit_process():
+    req = request.get_json()
+
+    return make_response(jsonify(req), 200)
+
 
 if __name__ == "__main__":
     app.run()
