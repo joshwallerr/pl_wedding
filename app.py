f3e2aedbfb6f33be9f0191ea9093d43fa12d9394
joshwallerr <joshwaller101@hotmail.com>
2022-08-18 19:17:23 +0100

    Can now recieve messages and store in list

--- app.py
+++ app.py
@@ -3,22 +3,35 @@ import sqlite3
 
 app = Flask(__name__)
 
+# 8 Min into video - https://www.youtube.com/watch?v=CHRikEvvcUc
+
+# REMEMBER TO STORE IN DB BEFORE PUBLISHING !!!!
+messages = [
+    ("Hope you have a great day!", "Jsh WAller"),
+    ("Hope you have a epic day!", "big man"),
+    ("Hope you have a nice day!", "grandma"),
+]
+
 @app.route("/")
 def index():
     message = {'message': "Have a great day!", 'from': "Josh"}
-    return render_template("index.html", message=message)
+    return render_template("index.html", messages=messages)
 
 
-@app.route("/submit")
+@app.route("/submit", methods=["POST", "GET"])
 def submit():
+    if request.method == 'POST':
+        print(request.form.get("message"))
+        print(request.form.get("from"))
+
+        messages.append(
+            (
+                request.form.get("message"),
+                request.form.get("from"),
+            )
+        )
+
     return render_template("submit.html")
-
-
-@app.route("/submit/process", methods=["POST", "GET"])
-def submit_process():
-    req = request.get_json()
-
-    return make_response(jsonify(req), 200)
 
 
 if __name__ == "__main__":
